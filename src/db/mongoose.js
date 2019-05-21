eabdf0cdaa2c6b86016cabd9f2c52542083f326b
Maplenix <senicesw@gmail.com>
2019-05-21 12:57:37 +0800

    mlab

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
+const mongoose = require('mongoose')
 try{
-    const mongoose = require('mongoose')
 
-    mongoose.connect('mongodb+srv://maplenix:admin@cluster0-zhtzh.mongodb.net/test?retryWrites=true', {
+    url = process.env.MONGOLAB_URI || 'mongodb+srv://maplenix:admin@cluster0-zhtzh.mongodb.net/test?retryWrites=true'
+    console.log(process.env.MONGOLAB_URI)
+    mongoose.connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false
