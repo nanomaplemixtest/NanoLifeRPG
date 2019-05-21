4daa20693f0f4fcfc365585a36eab21bccb02469
Maplenix <senicesw@gmail.com>
2019-05-21 12:29:04 +0800

    haiz

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,7 @@
 const mongoose = require('mongoose')
 
-const options = { 
-    server: { 
-      socketOptions: { 
-        keepAlive: 300000, connectTimeoutMS: 30000 
-      } 
-    }, 
-    replset: { 
-      socketOptions: { 
-        keepAlive: 300000, 
-        connectTimeoutMS : 30000 
-      } 
-    } 
-  };
-
-mongoose.connect('mongodb+srv://maplenix:admin@cluster0-zhtzh.mongodb.net/test?retryWrites=true', options)
+mongoose.connect('mongodb+srv://maplenix:admin@cluster0-zhtzh.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
